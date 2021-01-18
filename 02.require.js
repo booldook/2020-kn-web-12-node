556c6d71220b55ba46ea7c32ca5773bd844f978f
booldook <booldook@gmail.com>
2021-01-18 15:11:50 +0900

    ..

--- 02.require.js
+++ 02.require.js
@@ -2,6 +2,8 @@ const { sum } = require('./02.util');
 const http = require('http');
 const os = require('os');
 const fs = require('fs');
+const moment = require('moment');
+const _ = require('lodash');
 
 const server = http.createServer((req, res) => {
 	res.setHeader('Content-Type', 'text/html');
@@ -12,6 +14,10 @@ const server = http.createServer((req, res) => {
 // console.log( os.freemem() );
 console.log( fs.readFileSync('c:/test.txt').toString() );
 console.log( fs.copyFileSync('c:/test.txt', 'c:/test2.txt') );
+console.log( moment().format('YYYY-MM-DD hh:mm:ss') );
 
+const arr = [1,3,5,2,7,11,9,8];
+console.log(_.sortBy(arr));
+console.log(_.shuffle(arr));
 
 server.listen(1000, '127.0.0.1');
