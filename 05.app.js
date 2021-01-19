88dee63e0bf479313f3d4995b7d115fb8ccdf048
booldook <booldook@gmail.com>
2021-01-19 13:05:00 +0900

    ...

--- 05.app.js
+++ 05.app.js
@@ -12,17 +12,5 @@ app.listen(3000, () => { console.log('http://127.0.0.1:3000') });
 app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.use((req, res) => {
-	const html = `
-	<!doctype html>
-	<html>
-	<head>
-		<meta charset="utf-8">
-		<title>File not found</title>
-	</head>
-	<body style="margin: 10% 0 0 10%;">
-		<h1>Error 404</h1>
-		<p>파일을 찾을 수 없습니다.</p>
-	</body>
-	</html>`;
 	res.send(html);
 });
