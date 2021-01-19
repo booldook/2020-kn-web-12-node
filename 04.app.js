b01b2bfa53bdb1755bad8e92fad8133b0ff49519
booldook <booldook@gmail.com>
2021-01-19 10:23:32 +0900

    ...

--- 04.app.js
+++ 04.app.js
@@ -4,7 +4,22 @@ const app = express();
 app.listen(3000, () => { console.log('http://127.0.0.1:3000') });
 
 app.get('/', (req, res) => {
-	res.send('<h1>Hello Root</h1>');
+	var html = `
+	<!DOCTYPE html>
+	<html lang="ko">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>고골</title>
+	</head>
+	<body style="text-align: center; padding-top: 10em;">
+		<form method="get" action="/search">
+			<input type="text" name="q" style="padding: 0.5em;">
+			<button style="padding: 0.4em 3em;">검색</button>
+		</form>
+	</body>
+	</html>`;
+	res.send(html);
 });
 
 app.get('/booldook', (req, res) => {
