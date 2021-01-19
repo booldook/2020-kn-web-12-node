85c9da307ae9f069dfc40d0e61972a4d2120fb1f
booldook <booldook@gmail.com>
2021-01-19 16:58:54 +0900

    ...

--- 05.app.js
+++ 05.app.js
@@ -16,7 +16,14 @@ app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.get('/search', (req, res) => {
 	const q = req.query.q;
-	res.render('search', { q });
+	// 검색결과 로직 - 미구현
+	const lists = [
+		{title: `${q}물산`, content: `${q}물산, ${q}물산`, src: '/img/ny.jpg'},
+		{title: `${q}상사`, content: `${q}상사, ${q}상사`, src: '/img/paris.jpg'},
+		{title: `${q}약국`, content: `${q}약국, ${q}약국`, src: '/img/la.jpg'},
+		{title: `${q}식당`, content: `${q}식당, ${q}식당`, src: '/img/sanfran.jpg'}
+	];
+	res.render('search', { q, lists });
 });
 
 // http://127.0.0.1:3000/band?src=ny
