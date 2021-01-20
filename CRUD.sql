f334de0a1186bf5b100e84f124c9b09fb14ab192
booldook <booldook@gmail.com>
2021-01-20 17:26:24 +0900

    crud

---
+++ CRUD.sql
@@ -0,0 +1,17 @@
+-- Create(INSERT) Read(SELECT) Update(UPDATE) Delete(DELETE)
+
+-- INSERT INTO city SET 
+-- name='Incheon',
+-- country='Korea',
+-- summary='한국의 제3의 도시-수도권에 위치하고 있다.',
+-- lat=37.27,
+-- lon=126.42,
+-- population=2923000,
+-- sdate='1300-01-01';
+
+-- DELETE FROM city WHERE id=4;
+
+-- UPDATE city SET 
+-- summary='한국의 제2의 도시-도로가 좁다. 동백섬도 좋다.',
+-- population=2923012 
+-- WHERE id=2;
