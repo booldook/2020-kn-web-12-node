5ee0e4db11cec977e5b9c3e701b0f4a0874d619a
booldook <booldook@gmail.com>
2021-01-21 09:57:17 +0900

    ...

--- CRUD.sql
+++ CRUD.sql
@@ -15,3 +15,28 @@
 -- summary='한국의 제2의 도시-도로가 좁다. 동백섬도 좋다.',
 -- population=2923012 
 -- WHERE id=2;
+
+-- SELECT * FROM city; -- 모든데이터 가져오기(정렬을 해야한다.)
+-- SELECT * FROM city ORDER BY id DESC;-- id로 정렬해서 내림차순으로 가져오기
+-- SELECT * FROM city ORDER BY name ASC; -- name로 정렬해서 오름차순으로 가져오기
+-- SELECT name, country FROM city ORDER BY name ASC; -- 필요한 필드만 가져올 수 있다.
+-- SELECT COUNT(id) FROM city; -- 레코드(데이터)의 갯수 확인
+-- SELECT * FROM city WHERE name='pusan';
+-- SELECT id, name, population FROM city WHERE population > 2923000;
+-- SELECT id, name, population, lat FROM city WHERE population > 2923000 AND lat > 38;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
